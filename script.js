04cfa9a1bc93259d429270fec6aa5dfc0bd50b41
Dawa tashi Sherpa <dawatasi149@gmail.com>
2026-02-07 10:11:54 +0545

    make file more radable

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 const disp = document.getElementById("display");
 function appendToDisplay(input) {
-    disp.value += input;
+  disp.value += input;
 }
-function clearDisplay(){
-    disp.value = " ";
+function clearDisplay() {
+  disp.value = " ";
 }
 function calculate() {
-    try {
-        disp.value = eval(disp.value);   
-    }
-    catch (error) {
-        disp.value = "Error";
-   } 
-   
+  try {
+    disp.value = eval(disp.value);
+  } catch (error) {
+    disp.value = "Error";
+  }
 }
